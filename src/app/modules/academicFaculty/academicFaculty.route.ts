094ff5d5c06f88bfe07b40430aec76d0d467ac61
Koushik Ahmad <koushikahmad123@gmail.com>
2024-03-04 23:31:07 +0600

    added enrolled

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -17,7 +17,7 @@ router.post(
 );
 
 router.get(
-  '/:id',
+  '/:facultyId',
   auth(
     USER_ROLE.superAdmin,
     USER_ROLE.admin,
@@ -28,7 +28,7 @@ router.get(
 );
 
 router.patch(
-  '/:id',
+  '/:facultyId',
   auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(
     AcademicFacultyValidation.updateAcademicFacultyValidationSchema,
